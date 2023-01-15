2bbfc864458901c90314d1e887f33eef781638f4
Hélio Fernandes <104803451+HelioAnacronista@users.noreply.github.com>
2023-01-15 02:22:28 -0300

    fix: colors button

--- src/components/ButtonSubmit/index.tsx
+++ src/components/ButtonSubmit/index.tsx
@@ -6,7 +6,7 @@ export function ButtonSubmit({ nameBtn }: Props) {
   return (
     <button
       type="submit"
-      className=" text-white transition duration-300 bg-blue-600 hover:bg-fun-orange-400
+      className=" text-white-50 transition duration-300 bg-fun-blue-900 hover:bg-fun-orange-400
       font-medium rounded-lg 
       text-sm w-full px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
     >
--- src/components/HeaderClient/index.tsx
+++ src/components/HeaderClient/index.tsx
@@ -12,17 +12,16 @@ function HeaderClient() {
           <Link to={"/"} className="flex items-center">
             <img src={LogoIconImg} alt="" />
           </Link>
-          <div className="flex items-center md:order-2">
+          <div className="flex items-center ml-auto md:order-2">
             <Link to={"/login"}>
               <button className="text-gray-800 dark:text-white hover:bg-gray-100 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 py-2 md:px-5 md:py-2.5 mr-1 md:mr-2 dark:hover:bg-gray-700 focus:outline-none dark:focus:ring-gray-800">
                 Login
               </button>
             </Link>
-
             <Link to={"/createAccount"}>
               <button>
                 <a
-                  className="text-white transition duration-300 bg-blue-600 hover:bg-fun-orange-400
+                  className="text-white-50 transition duration-200 bg-fun-blue-900 hover:bg-fun-orange-400
                  font-medium rounded-lg text-sm px-4 py-2 md:px-5 md:py-2.5 mr-1 md:mr-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
                 >
                   Sign up
